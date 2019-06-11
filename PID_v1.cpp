3936ba4947761bb934be43056c41b8c5f9ecf11f
sodaflo <derecht@googlemail.com>
2019-06-11 21:57:10 +0200

    Update PID_v1.cpp
    corrects possible errors in larger projects.

--- PID_v1.cpp
+++ PID_v1.cpp
@@ -65,8 +65,8 @@ bool PID::Compute()
       /*Compute all the working error variables*/
       double input = *myInput;
       double error = *mySetpoint - input;
-      double dInput = (input - lastInput);
-      outputSum+= (ki * error);
+      double dInput = 1000*(input - lastInput)/timechange;
+      outputSum+= 1000*(ki * error)/timechange;
 
       /*Add Proportional on Measurement, if P_ON_M is specified*/
       if(!pOnE) outputSum-= kp * dInput;
